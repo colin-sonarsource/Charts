fc72ed7fe4274cc2158c40a7027a95ba3c48a7e2
colin-sonarsource <colin.mueller@sonarsource.com>
2024-06-18 08:51:37 +0200

    Update build.gradle.kts
    use kotlin dsl

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,13 @@ plugins {
     alias(libs.plugins.jetbrainsCompose) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.build.config) apply false
-    id "org.sonarqube" version "4.4.1.3373"
-
+    id("org.sonarqube") version "4.4.1.3373"
 }
 
 sonar {
   properties {
-    property "sonar.projectKey", "colin-sonarsource_Charts"
-    property "sonar.organization", "colin-sonarsource"
-    property "sonar.host.url", "https://sonarcloud.io"
+    property("sonar.projectKey", "colin-sonarsource_Charts")
+    property("sonar.organization", "colin-sonarsource")
+    property("sonar.host.url", "https://sonarcloud.io")
   }
 }
